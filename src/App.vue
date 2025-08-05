dbcbfefc9bbb20d1b6465093a96558837b01c434
Igor Stakic <stakic_igor@yahoo.com>
2025-08-05 23:13:39 +0200

    add single vote component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
+import SingleVote from './components/SingleVote.vue';
+import submissions from './seed.js'
+
 
 </script>
 
 <template>
-  up vote
+  <h2>Up Vote</h2>
+  {{ submissions[0].author }}
+  <SingleVote />
 </template>
 
 
---
+++ src/components/SingleVote.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h3>Single Vote</h3>
+        <img src="https://avatar.iran.liara.run/public/6">
+    </div>
+</template>
