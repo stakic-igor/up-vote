c3bf2a33dee2db257e1a817881ea7d7705055ca5
Igor Stakic <stakic_igor@yahoo.com>
2025-08-05 22:58:09 +0200

    add seed.js fake data file

---
+++ src/seed.js
@@ -0,0 +1,38 @@
+const submissions = [
+    {
+        id: 1,
+        title: "Yellow pail",
+        description: "lorem ipsum dolor sit amet consectetur adipiscing elit.",
+        url: "#",
+        votes: 16,
+        date: "03.12.2019",
+        author: "jessjelly",
+    },
+    {
+        id: 2,
+        title: "Green",
+        description: "lorem ipsum dolor sit amet consectetur adipiscing elit.",
+        url: "#",
+        votes: 5,
+        date: "05.11.2011",
+        author: "qazwsx",
+    },
+    {
+        id: 3,
+        title: "Purple wave",
+        description: "lorem ipsum dolor sit amet consectetur adipiscing elit.",
+        url: "#",
+        votes: 22,
+        date: "03.12.2023",
+        author: "katyperry",
+    },
+    {
+        id: 4,
+        title: "Blue",
+        description: "lorem ipsum dolor sit amet consectetur adipiscing elit.",
+        url: "#",
+        votes: 5,
+        date: "05.09.2024",
+        author: "beyonce",
+    }
+]
