1991fc77f9ddf7312e90c630b2f442ab4ddb777e
Igor Stakic <stakic_igor@yahoo.com>
2025-08-05 23:54:57 +0200

    basic style component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <script setup>
+import { onMounted } from 'vue';
 import SingleVote from './components/SingleVote.vue';
 import submissions from './seed.js'
 
 
+onMounted(() => {
+  
+});
+
 </script>
 
 <template>
-  <h2>Up Vote</h2>
-  {{ submissions[0].author }}
-  <SingleVote />
+  <h1 class="text-2xl text-center pt-2 pb-4">Up Vote</h1>
+  <SingleVote :submissions="submissions" />
 </template>
 
 
--- src/components/SingleVote.vue
+++ src/components/SingleVote.vue
@@ -1,10 +1,33 @@
 <script setup>
+const props = defineProps({
+    submissions: {
+        type: Array,
+        required: true
+    }
+});
+
+const upVote = (id) => {
+}
 
 </script>
 
 <template>
-    <div>
-        <h3>Single Vote</h3>
-        <img src="https://avatar.iran.liara.run/public/6">
+    <div class="border p-4 rounded" v-for="(submission, index) in submissions" :key="index">
+        <div class="flex items-center mb-4">
+            <img
+                class="w-16 h-16 rounded-full mb-4" 
+                :src="submission.avatar" 
+            />
+            <div>
+                <h3 class="text-lg">{{ submission.title }}</h3>
+                <p class="text-sm">{{ submission.description }}</p>
+            </div>
+            <div class="flex flex-col items-center ml-auto">
+                <span class="text-gray-500 text-sm">Votes: {{ submission.votes }}</span>
+                <button class="bg-blue-500 text-white px-2 py-1 rounded" @click="upVote(submission.id)">
+                    Vote
+                </button>
+            </div>
+        </div>
     </div>
 </template>
